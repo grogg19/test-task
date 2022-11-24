609c89e8acd5f5c8de4339a2998197a57c6a3690
grogg19 <differ@list.ru>
2022-11-25 02:09:37 +0300

    feat(controller): Сервис TaskArraysService подключен в контроллере  ExerciseController

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TaskArraysService;
 use App\Services\TaskStairsService;
 
 /**
@@ -14,10 +15,13 @@ class ExerciseController extends Controller
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function index(TaskStairsService $taskStairsService)
+    public function index(TaskStairsService $taskStairsService, TaskArraysService $taskArraysService)
     {
-        $numbers = $taskStairsService->getStructuredNumbersFromInterval(1, 1000);
+        $numbers = $taskStairsService->getStructuredNumbersFromInterval(1, 100);
+        
+        $taskArraysService->setSizeOfArray(5, 7);
+        $arrays = $taskArraysService->getStructuredArraysFromInterval(1, 1000);
 
-        return view('index', ['numbers' => $numbers]);
+        return view('index', ['numbers' => $numbers, 'arrays' => $arrays]);
     }
 }
