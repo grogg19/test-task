16528fee451e19ea041c42f37a73f2ef38b3c476
grogg19 <differ@list.ru>
2022-11-28 03:16:10 +0300

    fix(controller): добавлена выдача номера телефона локального подразделения в представление

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\TaskArraysService;
+use App\Services\TaskFrontService;
 use App\Services\TaskStairsService;
 
 /**
@@ -11,17 +12,20 @@ use App\Services\TaskStairsService;
 class ExerciseController extends Controller
 {
     /**
+     * Метод возвращает представление главной страницы
+     *
      * @param TaskStairsService $taskStairsService
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function index(TaskStairsService $taskStairsService, TaskArraysService $taskArraysService)
+    public function index(TaskStairsService $taskStairsService, TaskArraysService $taskArraysService, TaskFrontService $taskFrontService)
     {
         $numbers = $taskStairsService->getStructuredNumbersFromInterval(1, 100);
-        
-        $taskArraysService->setSizeOfArray(5, 7);
-        $arrays = $taskArraysService->getStructuredArraysFromInterval(1, 1000);
 
-        return view('index', ['numbers' => $numbers, 'arrays' => $arrays]);
+        $taskArraysService->setSizeOfArray(5, 7);
+        $arrays      = $taskArraysService->getStructuredArraysFromInterval(1, 1000);
+        $phoneNumber = $taskFrontService->getPhoneNumberByCity();
+
+        return view('index', ['numbers' => $numbers, 'arrays' => $arrays, 'phone_number' => $phoneNumber]);
     }
 }
